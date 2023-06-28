Fix averaging and add geometric average

PeercoinSim.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 
 dayyear=(365*33+8)/33
 secday=60*60*24
-MaxSimDays=365*2
 NumSim=1000
 
 #Simulation Variables
 diff=20.43
 StaticReward=1.34
+MaxSimDays=365*2
+geometric=True
 
 # Precomputed probability for 31-90 days to be adjusted by value/diff
 probsecs = [2**224 * (x+1) / (2**256) for x in range(60)]
@@ -36,9 +37,10 @@ def RandomDaysToMint(outValue,difficulty,rng):
 rng = np.random.default_rng()
 
 #Reward Wrapper
-def MintRewards(outValue,difficulty):
+def MintRewards(outValue, difficulty):
 
-    totalreward=0
+    totalreward = 1 if geometric else 0
+    totaldays = 0
 
     for _ in range(NumSim):
 
@@ -46,14 +48,23 @@ def MintRewards(outValue,difficulty):
 
         #Coinage Limit
         if MintDays < MaxSimDays:
-            CoinageReward=0.03*outValue*min(365, MintDays)/dayyear
-            normreward=(CoinageReward+StaticReward)/MintDays
-            totalreward+=normreward
+            reward=0.03*outValue*min(365, MintDays)/dayyear + StaticReward
+            if geometric:
+                totalreward *= 1+(reward/outValue)
+            else:
+                totalreward+=reward
+
+        # Add to total days the amount of time waited on this mint upto the
+        # maximum wait time
+        totaldays += min(MintDays, MaxSimDays)
 
     # Return annualised percentage
-    return totalreward/NumSim/outValue*36500
+    if geometric:
+        return (totalreward**(dayyear/totaldays) - 1)*100
+    rewardperday = totalreward/totaldays
+    return rewardperday/outValue*36500
 
-OutArray=[2**(x/2) for x in range(24)]
+OutArray=[2**(x/4) for x in range(50)]
 print(OutArray)
 
 def OutputWrapper():